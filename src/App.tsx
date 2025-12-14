6a0b51acf7c07e5b1fd6462f510751fb5a850218
VibeCoder67 <cedricchungtf@gmail.com>
2025-12-15 04:05:35 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home, About } from './pages';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/pages/About.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const About: React.FC = () => {
+    return (
+        <div style={{ padding: '20px' }}>
+            <h1>About Page</h1>
+            <p>This is the about page.</p>
+            <Link to="/">Go to Home</Link>
+        </div>
+    );
+};
+
+export default About;
