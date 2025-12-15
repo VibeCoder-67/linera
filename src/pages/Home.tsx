5c51108552f5b7efa6a976050307b355d417b08c
VibeCoder67 <cedricchungtf@gmail.com>
2025-12-15 17:51:36 +0800

    addec connect wallet component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import GridDistortion from '../components/GridDistortion';
 
+import ConnectWallet from '../components/ConnectWallet';
+
 const Home: React.FC = () => {
   return (
     <div className="relative w-full h-screen overflow-hidden">
@@ -13,6 +15,12 @@ const Home: React.FC = () => {
         relaxation={0.9}
         className="absolute top-0 left-0 w-full h-full z-0"
       />
+
+      {/* Wallet Connect Button */}
+      <div className="absolute top-4 right-4 z-20">
+        <ConnectWallet />
+      </div>
+
       <div className="relative z-10 p-5 text-white flex flex-col items-center justify-center h-full">
         <h1>Home Page</h1>
         <p>Welcome to the home page!</p>
