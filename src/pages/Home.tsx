83c5e4a2e0bc03358ce6c915389a94cad360cefd
VibeCoder67 <cedricchungtf@gmail.com>
2025-12-15 15:55:59 +0800

    applied gird distortion to landing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import GridDistortion from '../components/GridDistortion';
 
 const Home: React.FC = () => {
   return (
-    <div className="p-5 bg-[url('/background.png')] bg-cover bg-center min-h-screen text-white">
-      <h1>Home Page</h1>
-      <p>Welcome to the home page!</p>
-      <Link to="/about" className="text-[#61dafb]">Go to About</Link>
+    <div className="relative w-full h-screen overflow-hidden">
+      <GridDistortion
+        imageSrc="/background.png"
+        grid={20}
+        mouse={0.1}
+        strength={0.15}
+        relaxation={0.9}
+        className="absolute top-0 left-0 w-full h-full z-0"
+      />
+      <div className="relative z-10 p-5 text-white flex flex-col items-center justify-center h-full">
+        <h1>Home Page</h1>
+        <p>Welcome to the home page!</p>
+        <Link to="/about" className="text-[#61dafb]">Go to About</Link>
+      </div>
     </div>
   );
 };
